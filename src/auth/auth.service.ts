3015fe823bcc45581bf816b2222c182d9d9b3ccb
Tony Petrasinovic <tonypetrasinovic@gmail.com>
2024-05-01 22:22:04 +0200

    add token to response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,88 +1,99 @@
-import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.serive';
 import { AuthSigninDto, AuthSignupDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
-import {JwtService} from '@nestjs/jwt'
-import {jwtSecret} from '../utils/constants'
+import { JwtService } from '@nestjs/jwt';
+import { jwtSecret } from '../utils/constants';
 import { Request, Response } from 'express';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService, private jwt:JwtService) {
-        
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async signup(dto: AuthSignupDto) {
+    const { email, username, password } = dto;
+    const foundUser = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (foundUser) {
+      throw new BadRequestException('Email already exists');
+    }
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        username,
+        hashedPassword,
+      },
+    });
+    return { message: 'signup was succesfull' };
+  }
+  async signin(dto: AuthSigninDto, req: Request, res: Response) {
+    const { email, password } = dto;
+
+    const foundUser = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!foundUser) {
+      throw new BadRequestException('Wrong credentials');
     }
 
-    async signup(dto:AuthSignupDto) {
-        const {email, username, password} = dto;
-        const foundUser = await this.prisma.user.findUnique({
-            where: {
-                email
-            }
-        });
-        if (foundUser) {
-            throw new BadRequestException('Email already exists')
-        };
-        const hashedPassword = await this.hashPassword(password);
-
-        await this.prisma.user.create({
-            data: {
-                email,
-                username,
-                hashedPassword
-            }
-        });
-        return {message: 'signup was succesfull'};
-    }
-    async signin(dto:AuthSigninDto, req: Request, res: Response) {
-        const  {email, password} = dto;
-
-        const foundUser = await this.prisma.user.findUnique({
-            where: {
-                email
-            }
-        });
-        if (!foundUser) {
-            throw new BadRequestException('Wrong credentials')
-        };
-
-        const isMatch = await this.comparePasswords({
-            password,
-            hash: foundUser.hashedPassword,
-        });
-        if (!isMatch) {
-            throw new BadRequestException('Wrong credentials')
-        };
-
-        const token = await this.signToken({
-            id: foundUser.id, 
-            email: foundUser.email
-        });
-
-        if (!token) {
-            throw new ForbiddenException()
-        };
-        res.cookie('token', token);
-        return res.send({message: 'Logged in succefully'});
-    }
-    async signout(req: Request, res: Response) {
-        res.clearCookie('token')
-        return res.send({message: 'logged out succesfully'})
+    const isMatch = await this.comparePasswords({
+      password,
+      hash: foundUser.hashedPassword,
+    });
+    if (!isMatch) {
+      throw new BadRequestException('Wrong credentials');
     }
 
-    async hashPassword(password:string) {
-        const saltOrRounds = 10;
-        return await bcrypt.hash(password, saltOrRounds);
+    const token = await this.signToken({
+      id: foundUser.id,
+      email: foundUser.email,
+    });
+
+    if (!token) {
+      throw new ForbiddenException();
     }
+    res.cookie('token', token);
+    return res.send({
+      message: 'Logged in succefully',
+      user: {
+        id: foundUser.id,
+        email: foundUser.email,
+        username: foundUser.username,
+      },
+      token,
+    });
+  }
+  async signout(req: Request, res: Response) {
+    res.clearCookie('token');
+    return res.send({ message: 'logged out succesfully' });
+  }
 
-    async comparePasswords(args: {password: string, hash: string}) {
-        return await bcrypt.compare(args.password, args.hash);
-    }
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    return await bcrypt.hash(password, saltOrRounds);
+  }
 
-    async signToken(args: {id:string, email:string}) {
-        const payload = args;
+  async comparePasswords(args: { password: string; hash: string }) {
+    return await bcrypt.compare(args.password, args.hash);
+  }
 
-        return this.jwt.signAsync(payload, {secret: jwtSecret});
-    }
+  async signToken(args: { id: string; email: string }) {
+    const payload = args;
 
-
+    return this.jwt.signAsync(payload, { secret: jwtSecret, expiresIn: '1h' });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe( {whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
-  app.enableCors()
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
