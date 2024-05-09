1aed0ff6806973aab2a7e23ad9cc3fc463612501
Tony Petrasinovic <tonypetrasinovic@gmail.com>
2024-05-10 01:45:56 +0200

    remove log

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -4,13 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, user, info) {
-    console.log('info', info);
-    console.log('user', user);
-
     if (err || !user) {
-      console.log('error de test', err);
-      console.log(2);
-
       if (info && info.message === 'No auth token') {
         throw new UnauthorizedException({
           message: 'No auth token',
